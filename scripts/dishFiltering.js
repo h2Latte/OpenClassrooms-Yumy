10d699d1c9e3af900f4c7facfa76c8d1ee0aecfc
h2Latte <hugo.delatte@protonmail.com>
2023-12-10 18:47:09 +0100

    Refactor menu display and filtering code
    This commit simplifies and refactors the code related to menu display and filtering. The CSS for hiding and showing menu selections has been streamlined, and the JavaScript that controls these interactions has been compartmentalized into a distinct function for improved readability and maintainability. Furthermore, HTML code has been adjusted to align with the improved design.

--- scripts/dishFiltering.js
+++ scripts/dishFiltering.js
@@ -2,20 +2,15 @@ const filtersBtn = document.querySelectorAll(".menu__wrapper--filterbar button")
 const menuList = document.querySelectorAll(".menu__wrapper--choice ul");
 const filtersBtnArr = Array.from(filtersBtn);
 
-filtersBtn.forEach(button => {
-    button.addEventListener("click", (e) => {
-        filtersBtnArr[0].name = ""
-        {
-            let target = filtersBtnArr.indexOf(e.currentTarget)
-            console.log("here")
-            for (let i = 0; i < menuList.length; i++)
+function showMenu(e) {
+    menuList.forEach(menu => {
+        menu.classList.add("invisible");
+    });
+    let target = filtersBtnArr.indexOf(e.currentTarget)
+    menuList[target].classList.remove("invisible");
+}
 
-                menuList[i].classList.add("invisible"); //// Au click, tout devient invisible
-            menuList[0].classList.remove("menuVisible"); /// entree devient invisible
-
-
-            menuList[target].classList.replace("invisible", "menuVisible"); /// pour chaque target on remplace invisible par visible
-        }
-    })
-})
+filtersBtn.forEach(button =>
+    button.addEventListener("click", (e) => showMenu(e))
+)
 
